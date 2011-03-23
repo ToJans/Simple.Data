662b9007e39d2ce15523736b68d694a6bcb9e36c
Bobby Johnson <bobby.johnson@gmail.com>
2011-03-23 09:49:52 -0700

    first in memory test works!

--- Simple.Data.Sqlite/SqliteConnectionProvider.cs
+++ Simple.Data.Sqlite/SqliteConnectionProvider.cs
@@ -1,17 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.Composition;
-using System.Data.Common;
+using System.Data;
 using System.Data.SQLite;
-using System.Linq;
-using System.Text;
 using Simple.Data.Ado;
 using Simple.Data.Ado.Schema;
 
 namespace Simple.Data.Sqlite
 {
     [Export("db", typeof(IConnectionProvider))]
-    [Export("sql", typeof(IConnectionProvider))]
     public class SqliteConnectionProvider : IConnectionProvider
     {
         string _connectionString;
@@ -21,7 +17,7 @@ namespace Simple.Data.Sqlite
             _connectionString = connectionString;
         }
 
-        public DbConnection CreateConnection()
+        public virtual IDbConnection CreateConnection()
         {
             return new SQLiteConnection(_connectionString);
         }
@@ -54,6 +50,25 @@ namespace Simple.Data.Sqlite
         public IProcedureExecutor GetProcedureExecutor(AdoAdapter adapter, ObjectName procedureName)
         {
             throw new NotSupportedException("Sqlite does not support stored procedures.");
+        }
+    }
+
+    [Export("sql", typeof(IConnectionProvider))]
+    public class SqliteConnectionStringConnectionProvider : SqliteConnectionProvider
+    {
+        IDbConnection _connection;
+
+        public override IDbConnection CreateConnection()
+        {
+            if (ConnectionString.Contains(":memory:"))
+            {
+                if (_connection == null)
+                {
+                    return _connection = new SqliteInMemoryDbConnection(base.CreateConnection());
+                }
+                return _connection;
+            }
+            return base.CreateConnection();
         }
     }
 }
