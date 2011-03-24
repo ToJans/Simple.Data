685950051f4190f86e37e18c7277b82dcdd822d8
Bobby Johnson <bobby.johnson@gmail.com>
2011-03-24 07:45:29 -0700

    added nuget goo for sqlite package

--- NuGet/CopyReleases.cmd
+++ NuGet/CopyReleases.cmd
@@ -3,3 +3,4 @@ copy /y ..\Simple.Data.Ado\bin\Release\Simple.Data.Ado.dll Simple.Data.Ado\lib\n
 copy /y ..\Simple.Data.SqlServer\bin\Release\Simple.Data.SqlServer.dll Simple.Data.SqlServer\lib\net40
 copy /y ..\Simple.Data.SqlCe40\bin\Release\Simple.Data.SqlCe40.dll Simple.Data.SqlCompact40\lib\net40
 copy /y ..\Simple.Data.Mocking\bin\Release\Simple.Data.Mocking.dll Simple.Data.Mocking\lib\net40
+copy /y ..\Simple.Data.Sqlite\bin\Release\Simple.Data.Sqlite.dll Simple.Data.Sqlite\lib\net40
--- NuGet/NuGetPack.cmd
+++ NuGet/NuGetPack.cmd
@@ -3,3 +3,4 @@ nuget pack Simple.Data.Ado\Simple.Data.Ado.nuspec
 nuget pack Simple.Data.SqlServer\Simple.Data.SqlServer.nuspec
 nuget pack Simple.Data.SqlCompact40\Simple.Data.SqlCompact40.nuspec
 nuget pack Simple.Data.Mocking\Simple.Data.Mocking.nuspec
+nuget pack Simple.Data.Sqlite\Simple.Data.Sqlite.nuspec
